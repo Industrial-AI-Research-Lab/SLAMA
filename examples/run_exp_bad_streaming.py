0003191ff9ec570f42d7dcd0fcd9ff698f18b83a
fonhorst <alipoov.nb@gmail.com>
2025-01-04 17:33:09 +0300

    ---

--- examples/run_exp_bad_streaming.py
+++ examples/run_exp_bad_streaming.py
@@ -1,14 +1,28 @@
 import asyncio
+import logging
 from typing import Any, Dict
 
 import pandas as pd
 from kubernetes import client, config
+
+
+logger = logging.getLogger(__name__)
+
 
 NAMESPACE = "nbutakov"
 
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
+
+
+def clean_pods():
+    v1 = client.CoreV1Api()
+    ret = v1.list_namespaced_pod(namespace=NAMESPACE)
+    driver_pods = [pod for pod in ret.items if pod.metadata.name.endswith('-driver')]
+    for driver_pod in driver_pods:
+        logger.info(f"Deleting driver pod: {driver_pod}")
+        v1.delete_namespaced_pod(namespace=NAMESPACE, name=driver_pod.metadata.name)
 
 
 def get_exp_record(exp_name: str) -> Dict[str, Any]:
@@ -33,26 +47,34 @@ def get_exp_record(exp_name: str) -> Dict[str, Any]:
     }
 
 
-async def run_exp(exec_instances: int, exec_cores: int, dataset_name: str, exp_name: str) -> str:
-    max_cores = exec_instances * exec_cores
-    cmd = (f"REPO=node2.bdcl:5000 KUBE_NAMESPACE={NAMESPACE} "
-           f"SLAMA_EXEC_INSTANCES={exec_instances} SLAMA_EXEC_CORES={exec_cores} "
-           f"SLAMA_MAX_CORES={max_cores} SLAMA_RUN_NAME={exp_name} "
-           f"./bin/slamactl.sh submit-job-k8s "
-           f"examples/spark/spark-test-check-lightgbm.py {dataset_name}")
+async def run_exp(sem: asyncio.Semaphore,
+                  exec_instances: int,
+                  exec_cores: int,
+                  dataset_name: str,
+                  exp_name: str) -> str:
+    async with sem:
+        max_cores = exec_instances * exec_cores
+        cmd = (f"REPO=node2.bdcl:5000 KUBE_NAMESPACE={NAMESPACE} "
+               f"SLAMA_EXEC_INSTANCES={exec_instances} SLAMA_EXEC_CORES={exec_cores} "
+               f"SLAMA_MAX_CORES={max_cores} SLAMA_RUN_NAME={exp_name} "
+               f"./bin/slamactl.sh submit-job-k8s "
+               f"examples/spark/spark-test-check-lightgbm.py {dataset_name}")
 
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
+        proc = await asyncio.create_subprocess_shell(
+            cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
 
-    await proc.wait()
+        await proc.wait()
 
-    return exp_name
+        return exp_name
 
 
-async def main():
+async def main(max_concurrency: int = 10):
+    logger.info(f"Cleaning namespaces {NAMESPACE}")
+    clean_pods()
+
     datasets = [
         "lama_test_dataset",
         "company_bankruptcy_dataset",
@@ -73,15 +95,27 @@ async def main():
         for settings in spark_settings
     ]
 
-    tasks = [asyncio.create_task(run_exp(**config)) for config in configs]
+    sem = asyncio.Semaphore(10)
+
+    logger.info(f"Running experiments. Num experiments: {len(configs)}. Max concurrency: {max_concurrency}.")
+
+    tasks = [asyncio.create_task(run_exp(sem=sem, **config)) for config in configs]
 
     exp_names = await asyncio.gather(*tasks, return_exceptions=True)
 
+    logger.info("Finished experiments. Collecting results...")
+
     records = [get_exp_record(exp_name) for exp_name in exp_names if isinstance(exp_name, str)]
 
+    logger.info("Writing results as a DataFrame in json-format..")
     df = pd.DataFrame(records)
     df.to_json("streaming_runs_exps.json")
+    logger.info("All Finished")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(threadName)s %(levelname)s %(filename)s:%(lineno)d %(message)s"
+    )
     asyncio.run(main())
